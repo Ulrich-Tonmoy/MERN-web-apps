e2626fd7ecb63e3cb87c1c07eb40ec0f1f242b62
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2022-04-09 09:52:20 +0600

    posts routes model controller

--- social/server/index.js
+++ social/server/index.js
@@ -1,4 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from "cors"
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL =
+    "mongodb+srv://admin:$admin$@cluster0.lqmh7.mongodb.net/social?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() =>
+        app.listen(PORT, () => console.log(`Database connected and Server Running on Port ${PORT}`))
+    )
+    .catch((error) => console.log(error.message));
