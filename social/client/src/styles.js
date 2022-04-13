52bc22a9ca5bc83dd5b9cbd7fec22382181568ea
Ulrich-Tonmoy <ulrichtonmoy1@gmail.com>
2022-04-13 10:09:48 +0600

    style fixed
    main container mobile view centered elements

--- social/client/src/styles.js
+++ social/client/src/styles.js
@@ -18,6 +18,8 @@ export default makeStyles((theme) => ({
     [theme.breakpoints.down("sm")]: {
         mainContainer: {
             flexDirection: "column-reverse",
+            justifyContent: "center",
+            alignItems: "center",
         },
     },
 }));
