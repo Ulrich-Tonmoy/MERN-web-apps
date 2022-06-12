effffa8380d71b09548d59f0bf36dbb41d48a574
Tonmoy <ulrichtonmoy1@gmail.com>
2022-06-13 04:23:34 +0600

    cleaned unused code

--- social-blog/client/src/components/posts/post/Post.jsx
+++ social-blog/client/src/components/posts/post/Post.jsx
@@ -61,18 +61,6 @@ export default function Post({ post, setCurrentId }) {
                     <Typography variant="h6">{post.name}</Typography>
                     <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
                 </div>
-                {/* {(user?.result?.googleId === post?.creator ||
-                    user?.result?._id === post?.creator) && (
-                    <div className={classes.overlay2}>
-                        <Button
-                            style={{ color: "white" }}
-                            size="small"
-                            onClick={() => setCurrentId(post._id)}
-                        >
-                            <Edit fontSize="medium" />
-                        </Button>
-                    </div>
-                )} */}
                 <div className={classes.details}>
                     <Typography variant="body2" color="textSecondary">
                         {post.tags.map((tag) => `#${tag} `)}
