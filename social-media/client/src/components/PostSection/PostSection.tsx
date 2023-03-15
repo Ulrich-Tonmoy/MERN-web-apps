f91307f50de1740553133f39b893d5653d6bcc82
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-16 04:48:42 +0600

    🔨 post section

---
+++ social-media/client/src/components/PostSection/PostSection.tsx
@@ -0,0 +1,12 @@
+import "./PostSection.css";
+import { PostShare } from "..";
+
+const PostSection = () => {
+  return (
+    <div className="post-section">
+      <PostShare />
+    </div>
+  );
+};
+
+export default PostSection;
--- social-media/client/src/pages/Home/Home.tsx
+++ social-media/client/src/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
-import { LeftSection } from "../../components/index";
+import { LeftSection, PostSection } from "../../components";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <LeftSection />
-      <div className="center">Center</div>
+      <PostSection />
       <div className="right-bar">Right</div>
     </div>
   );
