f14ac43f7d14832e427acbc280d051b404c92c7a
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-17 04:48:02 +0600

    💄info card

---
+++ social-media/client/src/components/InfoCard/InfoCard.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { AiOutlineEdit } from "react-icons/ai";
+import "./InfoCard.css";
+
+const InfoCard = () => {
+  const [modalOpened, setModalOpened] = useState<Boolean>(false);
+  return (
+    <div className="info-card">
+      <div className="info-head">
+        <h4>Your Info</h4>
+        <div>
+          <AiOutlineEdit
+            style={{ width: "2rem", height: "1.5rem" }}
+            onClick={() => setModalOpened(true)}
+          />
+          {/* <ProfileModal modalOpened={modalOpened} setModalOpened={setModalOpened} /> */}
+        </div>
+      </div>
+      <div className="info">
+        <span>
+          <b>Status </b>
+        </span>
+        <span>in Relationship</span>
+      </div>
+      <div className="info">
+        <span>
+          <b>Lives in </b>
+        </span>
+        <span>Multan</span>
+      </div>
+      <div className="info">
+        <span>
+          <b>Works at </b>
+        </span>
+        <span>Zainkeepscode inst</span>
+      </div>
+      <button className="button logout-button">Logout</button>
+    </div>
+  );
+};
+
+export default InfoCard;
