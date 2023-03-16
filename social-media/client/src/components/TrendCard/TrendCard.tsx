bbb6d69a7f03704c7264a74753cabf069ce9b206
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-17 04:26:52 +0600

    💄trend card

---
+++ social-media/client/src/components/TrendCard/TrendCard.tsx
@@ -0,0 +1,20 @@
+import { TrendData } from "../../data/TrendData";
+import "./TrendCard.css";
+
+const TrendCard = () => {
+  return (
+    <div className="trend-card">
+      <h3>Trends for you</h3>
+      {TrendData.map((trend) => {
+        return (
+          <div className="trend">
+            <span>#{trend.name}</span>
+            <span>{trend.shares}k shares</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TrendCard;
