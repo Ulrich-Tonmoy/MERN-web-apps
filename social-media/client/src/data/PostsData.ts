41dbfc689b92183a7e13eccea08fb82fd0e6bcd1
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-17 04:11:24 +0600

    🗃️ post data update

--- social-media/client/src/data/PostsData.ts
+++ social-media/client/src/data/PostsData.ts
@@ -1,24 +1,20 @@
-import postPic1 from "/cover-default.png";
-import postPic2 from "/cover-default.png";
-import postPic3 from "/cover-default.png";
-
 export const PostsData = [
   {
-    img: postPic1,
+    img: "https://www.rappler.com/tachyon/2023/01/Momoland-Instagram-January-27.jpg",
     name: "Tzuyu",
     desc: "Happy New Year all friends! #2023",
     likes: 2300,
     liked: true,
   },
   {
-    img: postPic2,
+    img: "https://www.allkpop.com/upload/2023/01/content/270103/web_data/allkpop_1674799746_20230126-momoland.jpg",
     name: "Maryam",
     desc: "Party time :)",
     likes: 2300,
     liked: false,
   },
   {
-    img: postPic3,
+    img: "https://i1.sndcdn.com/artworks-000162168913-pxu3v4-t500x500.jpg",
     name: "Salena Gomez",
     desc: "At Archery Festival",
     likes: 800,
