0e71bf5149cb20f0ffb27075c32f0885ecab5808
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-17 05:11:49 +0600

    💄auth page

---
+++ social-media/client/src/pages/Auth/Auth.tsx
@@ -0,0 +1,19 @@
+import { Login } from "../../components";
+import "./Auth.css";
+
+const Auth = () => {
+  return (
+    <div className="auth">
+      <div className="auth-left">
+        <img src="/logo.png" alt="logo" />
+        <div className="web-name">
+          <h1>Social Media</h1>
+          <h6>Explore the ideas throughout the world</h6>
+        </div>
+      </div>
+      <Login />
+    </div>
+  );
+};
+
+export default Auth;
