af9013a0585393ed9dba691f56c3694999f71194
Tonmoy <ulrichtonmoy1@gmail.com>
2023-03-24 06:43:09 +0600

    🔨 user model

---
+++ social-media/server/models/userModel.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    fullName: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    profilePicture: String,
+    coverPicture: String,
+    about: String,
+    livesIn: String,
+    worksAt: String,
+    relationship: String,
+    followers: [],
+    followings: [],
+    lastLogin: Date,
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("Users", UserSchema);
+export default UserModel;
