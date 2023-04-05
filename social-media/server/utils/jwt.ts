7afbd77c9d98366565794e48e1479d0ea29b8c86
Tonmoy <ulrichtonmoy1@gmail.com>
2023-04-05 07:10:17 +0600

    🔨

--- social-media/server/utils/jwt.ts
+++ social-media/server/utils/jwt.ts
@@ -17,7 +17,6 @@ const attachCookiesToResponse = ({ res, user }) => {
   res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === "production",
     signed: true,
   });
 };
