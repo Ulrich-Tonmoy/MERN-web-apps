59bd834648a96f19c62977bb2b604fb7d8f55f13
Tonmoy <ulrichtonmoy1@gmail.com>
2023-04-05 06:41:09 +0600

    🔨 added utils script

---
+++ social-media/server/utils/index.ts
@@ -0,0 +1,4 @@
+import { createJWT, isTokenValid, attachCookiesToResponse } from "./jwt";
+import createTokenUser from "./createTokenUser";
+import checkPermissions from "./checkPermissions";
+export { createJWT, isTokenValid, attachCookiesToResponse, createTokenUser, checkPermissions };
