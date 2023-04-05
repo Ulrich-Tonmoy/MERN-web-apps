65ae574934597454db08bf29fd50663679eff526
Tonmoy <ulrichtonmoy1@gmail.com>
2023-04-05 07:09:19 +0600

    🔥

--- social-media/server/utils/checkPermissions.ts
+++
@@ -1,7 +0,0 @@
-const checkPermissions = (requestUser, resourceUserId) => {
-  if (requestUser.role === "admin") return;
-  if (requestUser.userId === resourceUserId.toString()) return;
-  return "Not authorized to access this route";
-};
-
-export default checkPermissions;
