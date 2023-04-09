c180ac6c4ba4f5104219c6d3d13e79ecc4562311
Tonmoy <ulrichtonmoy1@gmail.com>
2023-04-10 02:21:35 +0600

    ✨ posting api crud

--- social-media/server/controllers/postController.ts
+++ social-media/server/controllers/postController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import PostModel from "../models/postModel";
+import mongoose from "mongoose";
+import UserModel from "../models/userModel";
 
 export const createPost = async (req: Request, res: Response) => {
   const newPost = new PostModel(req.body);
@@ -7,6 +9,15 @@ export const createPost = async (req: Request, res: Response) => {
   try {
     await newPost.save();
     res.status(200).json({ response: newPost });
+  } catch (error) {
+    res.status(500).json({ response: error.message });
+  }
+};
+
+export const getAllPost = async (req: Request, res: Response) => {
+  try {
+    const post = await PostModel.find();
+    res.status(200).json({ response: post });
   } catch (error) {
     res.status(500).json({ response: error.message });
   }
@@ -23,7 +34,7 @@ export const getPost = async (req: Request, res: Response) => {
   }
 };
 
-export const getAllPostsByUser = async (req: Request, res: Response) => {
+export const getPostsByUser = async (req: Request, res: Response) => {
   const id = req.params.id;
 
   try {
@@ -31,5 +42,96 @@ export const getAllPostsByUser = async (req: Request, res: Response) => {
     res.status(200).json({ response: posts });
   } catch (error) {
     res.status(500).json({ response: error.message });
+  }
+};
+
+export const updatePost = async (req: Request, res: Response) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (post.userId === userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json({ response: post });
+    } else {
+      res.status(403).json({ response: "Action forbidden" });
+    }
+  } catch (error) {
+    res.status(500).json({ response: error.message });
+  }
+};
+
+export const deletePost = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(id);
+    if (post.userId === userId) {
+      await post.deleteOne();
+      res.status(200).json({ response: "Post deleted successfully" });
+    } else {
+      res.status(403).json({ response: "Action forbidden" });
+    }
+  } catch (error) {
+    res.status(500).json({ response: error.message });
+  }
+};
+
+export const likePost = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(id);
+    if (!post.likes.includes(userId)) {
+      await post.updateOne({ $push: { likes: userId } });
+      res.status(200).json({ response: "Post liked" });
+    } else {
+      await post.updateOne({ $pull: { likes: userId } });
+      res.status(200).json({ response: "Post UnLiked" });
+    }
+  } catch (error) {
+    res.status(500).json({ response: error.message });
+  }
+};
+
+export const getTimelinePosts = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const currentUserPosts = await PostModel.find({ userId: userId });
+    const followingUsersPosts = await UserModel.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "followings",
+          foreignField: "userId",
+          as: "followingUsersPosts",
+        },
+      },
+      {
+        $project: {
+          followingUsersPosts: 1,
+          _id: 0,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      response: currentUserPosts
+        .concat(...followingUsersPosts[0].followingUsersPosts)
+        .sort((a: any, b: any) => {
+          return b.createdAt - a.createdAt;
+        }),
+    });
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
--- social-media/server/routes/postRoutes.ts
+++ social-media/server/routes/postRoutes.ts
@@ -1,9 +1,23 @@
-import { createPost, getAllPostsByUser, getPost } from "../controllers/postController";
+import {
+  createPost,
+  deletePost,
+  getAllPost,
+  getPostsByUser,
+  getPost,
+  getTimelinePosts,
+  likePost,
+  updatePost,
+} from "../controllers/postController";
 import express from "express";
-const router = express.Router()
+const router = express.Router();
 
-router.post('/', createPost)
-router.get('/:id', getPost)
-router.get("/userPosts/:id", getAllPostsByUser)
+router.post("/", createPost);
+router.get("/", getAllPost);
+router.get("/:id", getPost);
+router.get("/userPosts/:id", getPostsByUser);
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.put("/like/:id", likePost);
+router.get("/timeline/:id", getTimelinePosts);
 
 export default router;
