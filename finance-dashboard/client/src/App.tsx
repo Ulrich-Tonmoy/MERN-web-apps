34ee39174ec76999561f3bc0b331cccac3b4d688
Tonmoy <ulrichtonmoy1@gmail.com>
2023-04-11 04:56:06 +0600

    🎉

---
+++ finance-dashboard/client/src/App.tsx
@@ -0,0 +1,27 @@
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "@/theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div>Dashboard</div>} />
+              <Route path="/predictions" element={<div>Predictions</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
