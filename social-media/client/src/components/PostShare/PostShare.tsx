8379a5370f3c6915075fd79517d1ec99155402ec
Tonmoy <ulrichtonmoy1@gmail.com>
2023-06-09 00:47:05 +0600

    Update PostShare.tsx

--- social-media/client/src/components/PostShare/PostShare.tsx
+++ social-media/client/src/components/PostShare/PostShare.tsx
@@ -4,21 +4,40 @@ import { SlCalender } from "react-icons/sl";
 import { AiOutlinePlayCircle, AiOutlineClose } from "react-icons/ai";
 import "./PostShare.css";
 import { ChangeEvent, useRef, useState } from "react";
-
-export interface Image {
-  image: string;
-}
+import { useDispatch, useSelector } from "react-redux";
 
 const PostShare = () => {
-  const [image, setImage] = useState<Image | null>(null);
+  const dispatch = useDispatch();
+  const [image, setImage] = useState<File | null>(null);
+  const [desc, setDesc] = useState<string>("");
   const imageRef = useRef<HTMLInputElement>(null);
+  const { user } = useSelector((state: any) => state.auth.authData);
 
   const onImageChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.files && event.target.files[0]) {
       let img = event.target.files[0];
-      setImage({
-        image: URL.createObjectURL(img),
-      });
+      setImage(img);
+    }
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const newPost: any = {
+      userId: user._id,
+      desc,
+    };
+    if (image) {
+      const data = new FormData();
+      const filename = crypto.randomUUID() + image.name;
+      data.append("name", filename);
+      data.append("file", image);
+      newPost.image = data;
+      console.log(newPost);
+      try {
+        // dispatch(uploadImage(newPost));
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
@@ -26,7 +45,14 @@ const PostShare = () => {
     <div className="post-share">
       <img src="/profile-default.png" alt="profile" />
       <div>
-        <input type="text" placeholder="What's happening?" />
+        <textarea
+          name="desc"
+          rows={6}
+          cols={50}
+          placeholder="What's happening?"
+          style={{ resize: "none" }}
+          onChange={(e) => setDesc(e.target.value)}
+        />
         <div className="post-options">
           <div
             className="option"
@@ -44,7 +70,9 @@ const PostShare = () => {
           <div className="option" style={{ color: "var(--schedule)" }}>
             <SlCalender /> Schedule
           </div>
-          <button className="button post-share__button">Share</button>
+          <button className="button post-share__button" onClick={handleSubmit}>
+            Share
+          </button>
           <div style={{ display: "none" }}>
             <input type="file" name="img" ref={imageRef} onChange={onImageChange} />
           </div>
@@ -52,7 +80,7 @@ const PostShare = () => {
         {image && (
           <div className="preview-image">
             <AiOutlineClose onClick={() => setImage(null)} />
-            <img src={image.image} alt="preview" />
+            <img src={URL.createObjectURL(image)} alt="preview" />
           </div>
         )}
       </div>
